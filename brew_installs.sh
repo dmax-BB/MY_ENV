db59c4f43f67eb1eb2f1f03c65d8d541b47faaa9
Dana Maxfield <dana.maxfield@gmail.com>
2017-07-14 13:51:57 -0400

    initial repo add

---
+++ brew_installs.sh
@@ -0,0 +1,24 @@
+##brew install findutils --default-names
+##brew install gnu-sed --default-names
+##brew install gnu-tar --default-names
+##brew install gnu-which --default-names
+##brew install gnutls --default-names
+##brew install grep --default-names
+##brew install coreutils
+##brew install binutils
+##brew install diffutils
+##brew install gzip
+##brew install watch
+##brew install tmux 
+##brew install wget
+##brew install nmap
+##brew install gpg
+##brew install htop
+##brew cask install java
+##brew install maven
+##brew install maven-completion
+##brew cask install netbeans
+##brew install groovy
+##brew install grails
+brew install gradle
+
